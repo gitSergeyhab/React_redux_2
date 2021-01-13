9e7277963bf6d065e49c0dac3347c3c4eb05d8aa
Sergey <redmoloch@yandex.ru>
2021-01-13 13:36:32 +0300

    homework complited

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import {MainPage, CartPage} from '../pages';
 import AppHeader from '../app-header';
-import WithRestoService from '../hoc'
+import WithRestoService from '../hoc';
+
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 
 import Background from './food-bg.jpg';
 
 const App = ({RestoService}) => {
     console.log(RestoService.getMenuItems())
     return (
-        <div style={{background: `url(${Background}) center center/cover no-repeat`}} className="app">
-            <AppHeader total={50}/>
-            <MainPage/>
-            <CartPage/>
-        </div>
+        <Router>
+            <div style={{background: `url(${Background}) center center/cover no-repeat`}} className="app">
+                <AppHeader total={50}/>
+                <Route exact path='/' component={MainPage}/>
+                <Route path='/cart' component={CartPage}/> 
+                {/* <MainPage/>
+                <CartPage/> */}
+            </div>
+        </Router>
     )
 }
 
--- src/services/resto-service.js
+++ src/services/resto-service.js
@@ -1,6 +1,6 @@
 
 
-let response = fetch('http://localhost:3000/menu').then(res=>res.json())
+let response = fetch('http://localhost:3002/menu').then(res=>res.json())
 
 export default class RestoServices {
     getMenuItems() {
